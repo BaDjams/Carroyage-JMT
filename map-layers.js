d954e0922b6f4f50f2dae007b5299183bb25c83f
BaDjams <romain.jamet@gmail.com>
2025-09-24 17:08:02 +0200

    Update map-layers.js

--- map-layers.js
+++ map-layers.js
@@ -1,52 +1,98 @@
 // map-layers.js
+const IGN_API_KEY = "ign_scan_ws";
 
 const MAP_LAYERS = [
     {
-        "id": "bing_hybrid",
-        "name": "Bing Hybrid",
+        "id": "ign_google_hybrid",
+        "name": "Ortho IGN + Routes Google",
+        "maxZoom": 20, // On se limite au zoom max de la couche IGN
         "layers": [
+            // Couche 1: Le fond de carte Ortho-imagerie de l'IGN (souvent en JPEG)
             {
-                "url": "http://ecn.t{s}.tiles.virtualearth.net/tiles/h{q}.jpeg?g=14927",
-                "type": "quadkey"
-            }
-        ]
-    },
-    {
-        "id": "osm_standard",
-        "name": "OSM Standard",
-        "layers": [
+                "url" : "https://data.geopf.fr/wmts?Layer=ORTHOIMAGERY.ORTHOPHOTOS&Style=normal&Timestamp=&TileMatrixSet=PM&SERVICE=WMTS&REQUEST=GetTile&Version=1.0.0&FORMAT=image/jpeg&TileMatrix={z}&TileCol={x}&TileRow={y}",
+                "type": "xyz"
+            },
+            // Couche 2: La surcouche de routes/étiquettes Google (fond transparent)
             {
-                "url": "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                "url": "https://mt0.google.com/vt/lyrs=h&hl=fr&x={x}&y={y}&z={z}&apistyle=s.t%3a2|s.e%3al|p.v%3aoff",
                 "type": "xyz"
             }
         ]
     },
     {
-        "id": "esri_satellite",
-        "name": "Satellite (Esri)",
+        "id": "google_hybrid",
+        "name": "Google Hybrid",
+        "maxZoom": 21,
         "layers": [
             {
-                "url": "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-                "type": "xyz_y_inverted" // Note: Esri utilise un ordre {y}/{x}
+                "url": "https://mt0.google.com/vt/lyrs=y&hl=fr&x={x}&y={y}&z={z}",
+                "type": "xyz"
             }
         ]
     },
-   /* {
-        "id": "ign_hybrid",
-        "name": "IGN Satellite + Routes (Exemple)",
+    {
+        "id": "google_hybrid_NOPOI",
+        "name": "Google Hybrid sans POI",
+        "maxZoom": 21,
         "layers": [
-            // Couche 1: Les images satellites (dessinées en premier)
             {
-                "url": "https://wxs.ign.fr/VOTRE_CLE_API/geoportail/wmts?layer=ORTHOIMAGERY.ORTHOPHOTOS&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image/jpeg&TileMatrix={z}&TileCol={x}&TileRow={y}",
-                "type": "xyz",
-                "attribution": "IGN-F/Geoportail"
-            },
-            // Couche 2: Les routes et étiquettes (dessinées par-dessus)
-            {
-                "url": "https://wxs.ign.fr//VOTRE_CLE_API/geoportail/wmts?layer=GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image/png&TileMatrix={z}&TileCol={x}&TileRow={y}",
-                "type": "xyz",
-                "attribution": "IGN-F/Geoportail"
+                "url": "https://mt0.google.com/vt/lyrs=y&hl=fr&x={x}&y={y}&z={z}&apistyle=s.t%3a2|s.e%3al|p.v%3aoff",
+                "type": "xyz"
             }
         ]
+    },
+    {
+        "id": "esri_hybrid",
+        "name": "Satellite Esri + Routes Google",
+        "maxZoom": 21, // Correspond au zoom élevé des deux sources
+        "layers": [
+            // Couche 1: Le fond de carte satellite Esri (fiable)
+            {
+                "url": "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+                "type": "xyz"
+            },
+            // Couche 2: La surcouche de routes/étiquettes OSM sur fond transparent (par CARTO)
+            {
+                "url": "https://mt0.google.com/vt/lyrs=h&hl=fr&x={x}&y={y}&z={z}&apistyle=s.t%3a2|s.e%3al|p.v%3aoff",
+				"type": "xyz"
+            }
+        ]
+    },
+	{
+        "id": "ign_public_hybrid",
+        "name": "Plan IGN",
+        "maxZoom": 20,
+        "layers": [
+            {
+                "url": "https://data.geopf.fr/wmts?Layer=GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2&Style=normal&TileMatrixSet=PM&SERVICE=WMTS&REQUEST=GetTile&Version=1.0.0&FORMAT=image/png&TileMatrix={z}&TileCol={x}&TileRow={y}",
+                "type": "xyz"
+            }
+        ]
+    },
+	{
+        "id": "osm_FR",
+        "name": "OSM Français",
+        "maxZoom": 19,
+        "layers": [
+            {
+                "url": "https://a.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png",
+                "type": "xyz"
+            }
+        ]
+    }/*,
+	{
+        id: "ign_scan25",
+        name: "IGN Scan 25",
+        maxZoom: 18,
+        type: 'wms', // Le type qui déclenchera la logique du plugin
+        url: 'https://data.geopf.fr/wms-r/wms',
+        wmsParams: {
+            apikey: IGN_API_KEY,
+            layers: 'SCAN25TOUR_PYR-JPEG_WLD_WM', // Notez 'layers' au lieu de 'LAYERS' pour le plugin
+            format: 'image/jpeg',
+            transparent: false,
+            version: '1.3.0',
+            crs: 'EPSG:4326'
+        }
     }*/
 ];
